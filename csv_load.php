b02698684b4958419f09bdefcc7361b5b042c347
efilatov <evgeny.filatov@caroperator.ru>
2017-05-11 06:43:12 +0300

    Done

--- csv_load.php
+++ csv_load.php
@@ -8,34 +8,34 @@
 $user = 'root';
 $pass = '13021962';
 $dbh = new PDO('mysql:host=localhost;dbname=kohana_test', $user, $pass);
-//$row = 1;
-//if (($handle = fopen("log_ip.csv", "r")) !== FALSE) {
-//    $b_id = $dbh->prepare("SELECT id FROM browsers WHERE descr=:descr");
-//    $o_id = $dbh->prepare("SELECT id FROM operation_systems WHERE descr=:os_descr");
-//    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-//        $row++;
-//        $b_id->execute(array('descr'=>$data[1]));
-//        $result_1 = $b_id->fetchColumn();
-//        $o_id->execute(array('os_descr'=>$data[2]));
-//        $result_2 = $o_id->fetchColumn();
-//        $sql = "INSERT INTO user_systems (ip_address,browser_id,operation_system_id) VALUES('".$data[0]."', ".$result_1.", ". $result_2.")";
-//        $dbh->query($sql);
-//    }
-//    fclose($handle);
-//}
-//$row = 1;
-//if (($handle = fopen("log_urls.csv", "r")) !== FALSE) {
-//    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-//        $row++;
-//        $sql_id = $dbh->prepare("SELECT id FROM user_systems WHERE ip_address=:ip_address");
-//        $sql_id->execute(array('ip_address'=>$data[2]));
-//        $result = $sql_id->fetchColumn();
-//        $sql = "INSERT INTO user_entrances (user_system_id,entrance_datatime,prev_url,entrance_url) VALUES(".$result.", '"
-//            .$data[0]." ".$data[1]."','".$data[3]."','".$data[4]."')";
-//        $dbh->query($sql);
-//    }
-//    fclose($handle);
-//}
+$row = 1;
+if (($handle = fopen("log_ip.csv", "r")) !== FALSE) {
+    $b_id = $dbh->prepare("SELECT id FROM browsers WHERE descr=:descr");
+    $o_id = $dbh->prepare("SELECT id FROM operation_systems WHERE descr=:os_descr");
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $row++;
+        $b_id->execute(array('descr'=>$data[1]));
+        $result_1 = $b_id->fetchColumn();
+        $o_id->execute(array('os_descr'=>$data[2]));
+        $result_2 = $o_id->fetchColumn();
+        $sql = "INSERT INTO user_systems (ip_address,browser_id,operation_system_id) VALUES('".$data[0]."', ".$result_1.", ". $result_2.")";
+        $dbh->query($sql);
+    }
+    fclose($handle);
+}
+$row = 1;
+if (($handle = fopen("log_urls.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $row++;
+        $sql_id = $dbh->prepare("SELECT id FROM user_systems WHERE ip_address=:ip_address");
+        $sql_id->execute(array('ip_address'=>$data[2]));
+        $result = $sql_id->fetchColumn();
+        $sql = "INSERT INTO user_entrances (user_system_id,entrance_datatime,prev_url,entrance_url) VALUES(".$result.", '"
+            .$data[0]." ".$data[1]."','".$data[3]."','".$data[4]."')";
+        $dbh->query($sql);
+    }
+    fclose($handle);
+}
 $res_sql = "SELECT us.ip_address AS ip_address, br.descr AS browser, os.descr AS operation_system,
 (SELECT ue.prev_url FROM user_entrances ue WHERE ue.user_system_id=us.id ORDER BY ue.entrance_datatime ASC LIMIT 1) AS first_prev_url,
 (SELECT ue.entrance_url FROM user_entrances ue WHERE ue.user_system_id=us.id ORDER BY ue.entrance_datatime DESC LIMIT 1) AS last_entrance_url,
